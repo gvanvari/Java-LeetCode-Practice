69463ce9f1447ff2c1ef7aadeff89b97b5a9cb17
gvanvari <gv591@nyu.edu>
2017-01-29 23:22:47 -0800

    Problem 155

--- MinStack.java
+++ MinStack.java
@@ -4,12 +4,10 @@ import java.util.Stack;
 
 public class MinStack {
 
-    /** initialize your data structure here. */
-	Stack<Integer> stack = new Stack<Integer>();
+    Stack<Integer> stack = new Stack<Integer>();
     Stack<Integer> stackMin = new Stack<Integer>();
+        
     public MinStack() {
-        
-        
     }
     
     public void push(int x) {
@@ -38,36 +36,5 @@ public class MinStack {
     public int getMin() {
         return stackMin.peek();
     }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(x);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
-
-    
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		MinStack ms = new MinStack();
-		ms.push(-2);
-		ms.push(0);
-		ms.push(-3);
-		
-		//ms.display();
-		
-		System.out.println("void main min  =" + ms.getMin());
-		ms.pop();
-		
-		//ms.display();
-		
-		System.out.println("void main top " + ms.top());
-		
-		System.out.println("void main min  =" + ms.getMin());
-		
-
-	}
+	
 }
