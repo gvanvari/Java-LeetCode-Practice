792baf1294897b693fc699fc08c5eebf1600d365
gvanvari <gv591@nyu.edu>
2017-01-29 23:56:29 -0800

    Problem 27

--- RemoveElement.java
+++ RemoveElement.java
@@ -1,42 +1,17 @@
 
 public class RemoveElement {
-	
-		/*public int removeElement(int[] nums, int val) {
-		    int i = 0;
-		    int n = nums.length;
-		    while (i < n) {
-		        if (nums[i] == val) {
-		            nums[i] = nums[n - 1];
-		            // reduce array size by one
-		            n--;
-		        } else {
-		            i++;
-		        }
-		    }
-		    return n;
-		}*/
-	
-	// buggy
-	
-	public int removeElement(int[] nums, int val){
-		int i=0;
-		int l= nums.length;
-		for(int j=1; j<l;j++){
-			if(nums[j]!=nums[i]){
-				i++;
-				nums[i] = nums[j];
-				
-			}
+	public int removeElement(int[] nums, int val) {
+	    int i = 0;
+	    int n = nums.length;
+	    while (i < n) {
+		if (nums[i] == val) {
+		    nums[i] = nums[n - 1];
+		    // reduce array size by one
+		    n--;
+		} else {
+		    i++;
 		}
-		return i;
+	    }
+	    return n;
 	}
-		
-	public static void main(String[] args) {
-		
-		int[] nums = {3,2,2,3};
-		RemoveElement re = new RemoveElement();
-		re.removeElement(nums, 3);
-		System.out.println(nums);
-	}
-
 }
