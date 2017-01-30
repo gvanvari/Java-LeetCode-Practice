c637fa4e595a9794e23b3e9709ab5d949127b65b
gvanvari <gv591@nyu.edu>
2017-01-29 23:17:21 -0800

    Problem 189

--- RotateArray.java
+++ RotateArray.java
@@ -1,33 +1,37 @@
 import java.util.ArrayList;
 
 public class RotateArray {
-
+	// runs in 1ms
+    public void rotate(int[] nums, int k) {
+        int[] a = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            a[(i + k) % nums.length] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = a[i];
+        }
+    }
+	// runs in 6ms
+	/*
 	public void rotate(int[] nums, int k) {
-        int l = nums.length;
+		int l = nums.length;
+
+		int modK = k % l;
+		 ArrayList<Integer> list = new ArrayList<Integer>();
+
+		    for(int i=0;i<l;i++){
+			list.add(i,nums[i]);
+		    }
+
+		    for(int i=l;i<2*l;i++){
+			list.add(i,nums[i-l]);
+		    }
+		    int k1=0;
+		    int n2=2*l;
+		    for(int i=l-modK;i<n2 - modK;i++){
+			nums[k1++] = list.get(i);
+		    }
         
-        int modK = k % l;
-        
-         ArrayList<Integer> list = new ArrayList<Integer>();
-        
-         for(int i=0;i<l;i++){
-             list.add(i,nums[i]);
-         }
-            
-         for(int i=l;i<2*l;i++){
-        	 System.out.println("A");
-             list.add(i,nums[i-l]);
-         }
-            
-         for(int i=0;i<l;i++){
-             nums[i] = list.get(i+modK+1);
-         }
-	}
-	
-	public static void main(String[] args) {
-		int[] nums = {1,2,3,4,5,6,7};
-		
-		RotateArray ra = new RotateArray();
-		ra.rotate(nums,3);
-	}
+    	}*/
 	
 }
