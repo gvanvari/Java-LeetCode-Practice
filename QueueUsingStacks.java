1cfd3bab3c17483a470cffb3a8887c078ae3ff34
gvanvari <gv591@nyu.edu>
2017-01-29 23:50:05 -0800

    Problem 232

---
+++ QueueUsingStacks.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public class QueueUsingStacks {
+		 
+	    private Stack<Integer> s1 = new Stack<>();
+	private Stack<Integer> s2 = new Stack<>();
+
+	int front;
+	 
+	    // Push element x to the back of queue.
+	    public void push(int x) {
+	        if (s1.empty())
+	            front = x;
+	        s1.push(x);
+	    }
+	 
+	    // Removes the element from in front of queue.
+	    public void pop() {
+	        if (s2.isEmpty()) {
+	            while (!s1.isEmpty())
+	                s2.push(s1.pop());
+	            }
+	        s2.pop(); 
+	    }
+	 
+	    // Get the front element.
+	    public int peek() {
+	       if (!s2.isEmpty()) {
+	            return s2.peek();
+	        }
+	        return front;
+	    }
+	 
+	    // Return whether the queue is empty.
+	    public boolean empty() {
+	        return s1.isEmpty() && s2.isEmpty();
+
+	    }
+
+}
