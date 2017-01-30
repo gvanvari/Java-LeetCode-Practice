5431a03262372a1eae37b8228bc8d3385683d7ea
gvanvari <gv591@nyu.edu>
2017-01-29 23:48:02 -0800

    Problem 172

--- FactorialTrailingZeros.java
+++ FactorialTrailingZeros.java
@@ -1,16 +1,13 @@
-
-public class TrailingZeros {
+public class FactorialTrailingZeros {
 
     public int trailingZeroes(int n) {
         
         int length = (int)(Math.log10(n)+1);
         
-        System.out.println(length);
-        
         int sum =0;
         int power;
         
-        for(int i=0;i<=length;i++){
+        for(int i=0;i<=length+2;i++){
             
             power = (int) Math.pow(5,i+1);
             sum = sum + n / power;
@@ -18,11 +15,5 @@ public class TrailingZeros {
         
         return sum;
     }
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		TrailingZeros tz = new TrailingZeros();
-		System.out.println(tz.trailingZeroes(1808548329));
-	}
 
 }
